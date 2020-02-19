3569e9619b4f00c547cb3048b913b0d3d7a6583e
github username <ingrey1@gmail.com>
2020-02-19 10:10:48 -0800

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,62 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (newType) => {
+      console.log(newType.target.value)
+      newType.persist()
+      this.setState((prevState) => {
+        return {
+        ...prevState,
+        filters: {
+          ...prevState.filters,
+          type: newType.target.value
+        }
+      }
+    })
+      
+      
+  }
+
+  onAdoptPet = (id) => {
+    // find the pet
+    // make a copy of the pet
+    // set the state
+      // set the pets key equal to a new array
+        //* in the new array, concat it with a the copy of pet put in an array literal
+    
+    const pet = this.state.pets.find(pet => pet.id === id)
+    if (!pet.isAdopted) {
+    this.setState((prevState => {
+      return {
+        ...prevState,
+        pets: prevState.pets.map(pet => {
+          if (pet.id === id ) {
+             return {
+               ...pet,
+               isAdopted: true
+             }  
+          } else {
+            return pet            
+          }
+        })
+      }
+    }))
+  }
+
+  }
+
+  onFindPetsClick = () => {
+    let url = "/api/pets";
+    if (this.state.filters.type === "cat") url += `?type=cat`;
+    else if (this.state.filters.type === "dog") url += `?type=dog`;
+    else if (this.state.filters.type === "micropig") url += `?type=micropig`;
+    console.log(url)
+    return fetch(url).then(resp => resp.json()).then(pets => this.setState({
+      ...this.state,
+      pets: pets
+    }))
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +80,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.onAdoptPet} pets={this.state.pets} />
             </div>
           </div>
         </div>
